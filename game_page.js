82cc8006bfb3bbd9112c708e51c021d6da0ec744
rafiguadalupe <83619619+rafiguadalupe@users.noreply.github.com>
2021-11-13 16:28:23 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -10,7 +10,31 @@ document.getElementById("player_question").innerHTML="Question turn"+player1_nam
 document.getElementById("player_answer").innerHTML="Answer turn"+player1_answer;
 
 
-function send(){
+function check()
+{
+get_answer = document.getElementById("input_check_box").value;
+if(get_answer == actual_answer)
+{
+    if(answer_turn == "player1")
+{
+update_player1_score = player1_score +1;
+document.getElementById("player1_score").innerHTML = update_player1_score;
+}
+else
+{
+update_player2_score = player2_score +1;
+document.getElementById("player2_score").innerHTML = update_player2_score; 
+}
 
+}
+if(question_turn == "player1")
+{
+question_turn = "player2";
+document.getElementById("player_question").innerHTML = "Question Turn-" + player2_name;
+}
+else
+{
+    question_turn = "player1";
+    document.getElementById("player_question").innerHTML = "Question Turn-" + player1_name;
 
-} 
+}}
